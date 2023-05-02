e324ef48c41fee42a4885401dd323d96d4f4f358
NehamaMandelbaum <nehama1836@gmail.com>
2023-05-02 11:22:52 -0300

    style: themes and colours

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,20 @@
+export const defaultTheme = {
+  white: "#FFF",
+
+  "gray-100": "#fafafa",
+  "gray-200": "#f3f2f2",
+  "gray-300": "#ededed",
+  "gray-400": "#e6e5e5",
+  "gray-500": "#d7d5d5",
+  "gray-600": "#8d8686",
+  "gray-700": "#574f4d",
+  "gray-800": "#403937",
+  "gray-900": "#272221",
+
+  "purple-700": "#4B2995",
+  "purple-500": "#8047F8",
+  "purple-300": "#EBE5F9",
+  "yellow-700": "#C47F17",
+  "yellow-500": "#DBAC2C",
+  "yellow-300": "#F1E9C9",
+};
