036e37f2b74472d3e979fa64c22eb52797ce5dfe
NehamaMandelbaum <nehama1836@gmail.com>
2023-06-06 12:20:19 -0300

    feat: html for checkout page

--- src/pages/Checkout.tsx
+++ src/pages/Checkout.tsx
@@ -1,3 +1,50 @@
 export function Checkout() {
-  return <h1>Checkout</h1>;
+  return (
+    <main>
+      <form>
+        <div>
+          <h2>Complete seu pedido</h2>
+          <div>
+            <h3>Endereço de Entrega</h3>
+            <p>Informe o endereço onde deseja receber seu pedido</p>
+          </div>
+          <div>
+            <input placeholder="CEP" type="text" />
+            <input placeholder="Rua" type="text" />
+            <input placeholder="Número" type="text" />
+            <input placeholder="Complemento" type="text" />
+            <input placeholder="Bairro" type="text" />
+            <input placeholder="Cidade" type="text" />
+            <input placeholder="UF" type="text" />
+          </div>
+          <div>
+            <h3>Pagamento</h3>
+            <p>
+              O pagamento é feito na entrega. Escolha a forma que deseja pagar
+            </p>
+          </div>
+          <div>
+            <button type="button">CARTÃO DE CRÉDITO</button>
+            <button type="button">CARTÃO DE DÉBITO</button>
+            <button type="button">DINHEIRO</button>
+          </div>
+        </div>
+        <div>
+          <h2>Cafés selecionados</h2>
+          <div>
+            <div> //Itens do carrinho aqui</div>
+          </div>
+          <div>
+            <span>Total de Itens</span>
+            <span>R$ 29,70</span>
+            <span>Entrega </span>
+            <span>R$ 3,50</span>
+            <span>Total</span>
+            <span>R$ 33,20</span>
+          </div>
+        </div>
+        <button type="submit"> Confirmar pedido</button>
+      </form>
+    </main>
+  );
 }
